994c00cae970a2795b146dffba70085342fda24a
Cristian Castro V <cristian1853@gmail.com>
2017-04-27 16:32:02 -0500

    Remove comments.

--- alb/main.tf
+++ alb/main.tf
@@ -1,10 +1,10 @@
 resource "aws_alb" "alb" {
-  name            = "${var.name}"             # String
-  internal        = "${var.internal}"         # Boolean
-  security_groups = ["${var.security_groups}"]  # Array
-  subnets         = ["${var.subnets}" ]         # Aray
-  idle_timeout    = "${var.idle_timeout}"     # Integer
-  enable_deletion_protection = "${var.enable_deletion_protection}" # Boolean
+  name            = "${var.name}"             
+  internal        = "${var.internal}"         
+  security_groups = ["${var.security_groups}"]  
+  subnets         = ["${var.subnets}"]         
+  idle_timeout    = "${var.idle_timeout}"     
+  enable_deletion_protection = "${var.enable_deletion_protection}" 
 
   
 
