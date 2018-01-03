bcb1cddd2e83f5117cda606ac4260fe0d3015ebc
Cristian Castro V <cristian1853@gmail.com>
2018-01-03 14:15:44 -0500

    Add to outputs the ALB listener ARN.

--- alb_listener_tg/outputs.tf
+++ alb_listener_tg/outputs.tf
@@ -1,3 +1,7 @@
 output "target_group_arn" {
-	value = "${aws_alb_target_group.target_group.arn}"
+  value = "${aws_alb_target_group.target_group.arn}"
+}
+
+output "listener_arn" {
+  value = "${aws_alb_listener.listener.arn}"
 }
