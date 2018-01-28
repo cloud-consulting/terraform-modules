232e901ad8799266258499c4409c48fe9ee50e26
Cristian Castro V <cristian1853@gmail.com>
2018-01-28 13:47:39 -0500

    Fix ecs_sevice call to the ecs_service_arn output to use terraform v0.11.2.

--- ecs_service/outputs.tf
+++ ecs_service/outputs.tf
@@ -1,19 +1,19 @@
 output "ecs_service_arn" {
-	value = "${aws_ecs_service.service.arn}"
+  value = "${aws_ecs_service.service.id}"
 }
 
 output "ecs_service_name" {
-	value = "${aws_ecs_service.service.name}"
+  value = "${aws_ecs_service.service.name}"
 }
 
 output "ecs_service_cluster" {
-	value = "${aws_ecs_service.service.cluster}"
+  value = "${aws_ecs_service.service.cluster}"
 }
 
 output "ecs_service_iam_role" {
-	value = "${aws_ecs_service.service.iam_role}"
+  value = "${aws_ecs_service.service.iam_role}"
 }
 
 output "ecs_service_iam_desired_count" {
-	value = "${aws_ecs_service.service.desired_count}"
+  value = "${aws_ecs_service.service.desired_count}"
 }
